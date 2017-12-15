000ac6ee3c56ca7a9f878b9c26ac7d9a84372888
Francisco Valente <fco.valente.castro@gmail.com>
2017-12-15 04:24:45 -0500

    Update useRipser.R

--- useRipser.R
+++ useRipser.R
@@ -1,18 +1,9 @@
-# Operate on a data point cloud represented as a matrix
+# Import library  
 source( 'Ripser.R' )
 
+# Load matrix of points of size n x 3
 m = torus
-#M = SDataSet3Matrix_F
 
 # Point cloud
 diaG = ripserDiag( m , dimension = 2 , threshold = 2 )
 plot.diagram( diaG )
-
-# Distance matrix
-
-## Fix negative numbers in matrix
-M[ M == -1 ] = .00000001
-
-## Calculate homology
-diAG = ripserDiag( M , dimension = 1 , threshold = 1.5, format = "distance" )
-plot.diagram( diAG )
